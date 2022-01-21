1952548690a4a56a9e5a85a6444d26cb5fc138b2
aobatact <aobatact144@gmail.com>
2022-01-21 11:10:18 +0900

    refactor de/arg

--- src/de/arg.rs
+++ src/de/arg.rs
@@ -16,77 +16,59 @@ impl<'a> Visitor<'a> for ArgVisitor<'a> {
         A: serde::de::MapAccess<'a>,
     {
         let mut arg = Arg::new(self.0);
-        macro_rules! parse_value {
-            ( $app : ident, $map : ident, $value_type:ty, $register : ident) => {
-                Arg::$register($app, $map.next_value::<$value_type>()?)
-            };
-            ( $app : ident, $map : ident, ref $value_type:ty, $register : ident) => {
-                Arg::$register($app, &$map.next_value::<$value_type>()?)
-            };
-        }
 
         //TODO: handle_vec_or_str
         while let Some(key) = map.next_key::<&str>()? {
-            arg = match key {
-                "short" => parse_value!(arg, map, char, short),
-                "long" => parse_value!(arg, map, &str, long),
-                "aliases" => parse_value!(arg, map, ref Vec<&str>, aliases), //TODO: no alloc
-                "help" => parse_value!(arg, map, &str, help),
-                "long_help" => parse_value!(arg, map, &str, long_help),
-                "required" => parse_value!(arg, map, bool, required),
-                "required_if_eq" => todo!(), //parse_value!(arg, map, ),
-                "takes_value" => parse_value!(arg, map, bool, takes_value),
-                "index" => parse_value!(arg, map, usize, index),
-                "global" => parse_value!(arg, map, bool, global),
-                "multiple_occurences" => parse_value!(arg, map, bool, multiple_occurrences),
-                "multiple_values" => parse_value!(arg, map, bool, multiple_values),
-                "hide" => parse_value!(arg, map, bool, hide),
-                "next_line_help" => parse_value!(arg, map, bool, next_line_help),
-                "group" => parse_value!(arg, map, &str, group),
-                "number_of_values" => parse_value!(arg, map, usize, number_of_values),
-                "max_values" => parse_value!(arg, map, usize, max_values),
-                "min_values" => parse_value!(arg, map, usize, min_values),
-                "value_name" => parse_value!(arg, map, &str, value_name),
-                "use_delimiter" => parse_value!(arg, map, bool, use_delimiter),
-                "allow_hyphen_values" => parse_value!(arg, map, bool, allow_hyphen_values),
-                "last" => parse_value!(arg, map, bool, last),
-                "require_delimiter" => parse_value!(arg, map, bool, require_delimiter),
-                "value_delimiter" => parse_value!(arg, map, char, value_delimiter),
-                "required_unless_present" => parse_value!(arg, map, &str, required_unless_present),
-                "display_order" => parse_value!(arg, map, usize, display_order),
-                "default_value" => parse_value!(arg, map, &str, default_value),
-                "default_value_if" => todo!(),
-                "default_value_ifs" => todo!(),
-                #[cfg(env)]
-                "env" => parse_value!(arg, map, &str, env),
-                "value_names" => parse_value!(arg, map, ref Vec<&str>, value_names),
-                "groups" => parse_value!(arg, map, ref Vec<&str>, groups),
-                "requires" => parse_value!(arg, map, &str, requires),
-                "requires_if" => todo!(), //parse_value!(arg, map, &str, requires_if),
-                "requires_ifs" => todo!(),
-                "conflicts_with" => parse_value!(arg, map, &str, conflicts_with),
-                "overrides_with" => parse_value!(arg, map, &str, overrides_with),
-                "possible_values" => parse_value!(arg, map, Vec<&str>, possible_values),
-                "case_insensitive" => parse_value!(arg, map, bool, ignore_case),
-                "required_unless_present_any" => {
-                    parse_value!(arg, map, ref Vec<&str>, required_unless_present_any)
-                }
-                "requierd_unless_present_all" => {
-                    parse_value!(arg, map, ref Vec<&str>, required_unless_present_all)
-                }
-                dep @ ("required_if"
-                | "required_ifs"
-                | "multiple"
-                | "required_unless"
-                | "required_unless_one"
-                | "required_unless_all") => {
-                    return Err(Error::custom(format_args!(
-                        "deprecated arg field : {}",
-                        dep
-                    )))
-                }
-                unkonw => return Err(Error::unknown_field(unkonw, &[""])),
-            }
+            arg = parse_value!(key, arg, map, Arg, {
+                (char, short),
+                (&str, long),
+                (ref Vec<&str>, aliases),
+                (&str, help),
+                (&str, long_help),
+                (bool, required),
+                (bool, takes_value),
+                (usize, index),
+                (bool, global),
+                (bool, multiple_values),
+                (bool, hide),
+                (bool, next_line_help),
+                (&str, group),
+                (usize, number_of_values),
+                (usize, max_values),
+                (usize, min_values),
+                (&str, value_name),
+                (bool, use_delimiter),
+                (bool, allow_hyphen_values),
+                (bool, last),
+                (bool, require_delimiter),
+                (char, value_delimiter),
+                (&str, required_unless_present),
+                (usize, display_order),
+                (&str, default_value),
+                (ref Vec<&str>, value_names),
+                (ref Vec<&str>, groups),
+                (&str, requires),
+                (&str, conflicts_with),
+                (&str, overrides_with),
+                (Vec<&str>, possible_values),
+                (bool, ignore_case),
+                (ref Vec<&str>, required_unless_present_any),
+                (ref Vec<&str>, required_unless_present_all),
+                //     "default_value_if" => todo!(),
+                //     "default_value_ifs" => todo!(),
+                //     #[cfg(env)]
+                //     "env" => parse_value!(arg, map, &str, env),
+                //     "requires_if" => todo!(), //parse_value!(arg, map, &str, requires_if),
+                //     "requires_ifs" => todo!(),
+            },
+            deprecated:
+            [
+                "required_if",
+                "multiple",
+                "required_unless",
+                "required_unless_one",
+                "required_unless_all",
+            ]);
         }
         Ok(ArgWrap { arg })
     }
